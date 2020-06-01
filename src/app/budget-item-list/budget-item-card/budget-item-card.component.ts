54a83783bb100c2b67051a0a9db54725160e4f56
Michael <michael.rolle@hotmail.com>
2020-06-01 15:46:19 +0200

    backup from local

---
+++ src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { BudgetItem } from 'src/shared/models/budget-item.model';
+
+@Component({
+  selector: 'app-budget-item-card',
+  templateUrl: './budget-item-card.component.html',
+  styleUrls: ['./budget-item-card.component.scss']
+})
+export class BudgetItemCardComponent implements OnInit {
+
+  @Input() item: BudgetItem;
+  @Output() xButtonClick: EventEmitter<any> = new EventEmitter<any>();
+  @Output() cardClick: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onXButtonClick() {
+    //emit event
+    this.xButtonClick.emit();
+  }
+
+  onCardClicked() {
+    this.cardClick.emit();
+  }
+}
---
+++ src/app/budget-item-list/budget-item-list.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { BudgetItem } from 'src/shared/models/budget-item.model';
+import { MatDialog } from '@angular/material';
+import { EditItemModalComponent } from '../edit-item-modal/edit-item-modal.component';
+
+@Component({
+  selector: 'app-budget-item-list',
+  templateUrl: './budget-item-list.component.html',
+  styleUrls: ['./budget-item-list.component.scss']
+})
+
+
+export class BudgetItemListComponent implements OnInit {
+
+  @Input() budgetItems: BudgetItem[];
+  @Output() delete: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
+  @Output() update: EventEmitter<UpdateEvent> = new EventEmitter<UpdateEvent>();
+
+  constructor(public dialog: MatDialog) { }
+
+  ngOnInit() {
+  }
+
+  onDeleteButtonClicked(item: BudgetItem) {
+    this.delete.emit(item);
+  }
+
+  onCardClicked(item: BudgetItem) {
+    //show edit modal
+    const dialogRef = this.dialog.open(EditItemModalComponent, {
+      width: '580px',
+      data: item
+    });
+
+    dialogRef.afterClosed().subscribe(res => {
+      //check if result has value
+      if (res) {
+        //replace item with updated/submitted item from the form
+        // this.budgetItems[this.budgetItems.indexOf(item)] = res;
+
+        this.update.emit({ old: item, new: res });
+      }
+    })
+  }
+
+}
+
+
+export interface UpdateEvent {
+  old: BudgetItem;
+  new: BudgetItem;
+}
+
